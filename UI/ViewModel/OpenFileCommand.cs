5ac224433cc7a723f98b4cf3161de9e15c2f0599
David1Savitsky <omgprogress24@gmail.com>
2022-10-24 21:16:02 +0300

    Initial commit

---
+++ UI/ViewModel/OpenFileCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace UI.ViewModel;
+
+public class OpenFileCommand : ICommand
+{
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
+        
+    public OpenFileCommand(Action<object> execute, Func<object, bool> canExecute = null)
+    {
+        this._execute = execute;
+        this._canExecute = canExecute;
+    }
+        
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute == null || _canExecute(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }    
+}
