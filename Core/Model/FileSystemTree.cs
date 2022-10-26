2c5d40f29c6c54cfc33be3b05a0f2878755ef967
David1Savitsky <omgprogress24@gmail.com>
2022-10-27 01:12:19 +0300

    Create core models

---
+++ Core/Model/FileSystemTree.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Model;
+
+public class FileSystemTree
+{
+    private Node Root { get; }
+    
+    public FileSystemTree(Node root)
+    {
+        Root = root;
+    }
+}
---
+++ Core/Model/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Core.Model;
+
+public class Node
+{
+    public string Name { get; }
+    public string FullPath { get; }
+    public long Size { get; set; }
+    public ObservableCollection<Node> ChildrenNodes { get; }
+
+    public Node(string name, string fullPath, long size)
+    {
+        Name = name;
+        FullPath = fullPath;
+        Size = size;
+        ChildrenNodes = new ObservableCollection<Node>();
+    }
+
+    public Node(string name, string fullPath)
+    {
+        Name = name;
+        FullPath = fullPath;
+        ChildrenNodes = new ObservableCollection<Node>();
+    }
+}
