9ceba8393e219dcc35350964956fedffa66788cb
David1Savitsky <omgprogress24@gmail.com>
2022-10-29 17:43:25 +0300

    Add parent property

--- Core/Model/Node.cs
+++ Core/Model/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 
 namespace Core.Model;
 
@@ -7,20 +8,24 @@ public class Node
     public string Name { get; }
     public string FullPath { get; }
     public long Size { get; set; }
+    [JsonIgnore]
+    public Node Parent { get; }
     public ObservableCollection<Node> ChildrenNodes { get; }
 
-    public Node(string name, string fullPath, long size)
+    public Node(string name, string fullPath, long size, Node parent)
     {
         Name = name;
         FullPath = fullPath;
         Size = size;
+        Parent = parent;
         ChildrenNodes = new ObservableCollection<Node>();
     }
 
-    public Node(string name, string fullPath)
+    public Node(string name, string fullPath, Node parent)
     {
         Name = name;
         FullPath = fullPath;
+        Parent = parent;
         ChildrenNodes = new ObservableCollection<Node>();
     }
 }
